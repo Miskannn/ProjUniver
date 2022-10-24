73836f5d8ce86cd8afe59a608d2a92561856daaf
Miskannn <maksim.kovtun2103@gmail.com>
2022-10-24 17:17:31 +0300

    Добавьте файлы проекта.

---
+++ ProjUniver.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+double findArea(double d)
+{
+    return (d * d) / 2.0;
+}
+
+int main()
+{
+    double d = 10;
+    cout << (findArea(d));
+    return 0;
+}
